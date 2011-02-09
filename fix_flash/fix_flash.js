3ae780736ebb50107ea6ad3d5d320466077e9900
Harris Novick <harris.novick@sonymusic.com>
2011-02-09 17:54:59 -0500

    Added comprehensive fix_flash js for IE67, IE8, and all others.

---
+++ fix_flash/fix_flash.js
@@ -0,0 +1,28 @@
+window.fix_wmode2transparent_swf = function () {
+	if(typeof (jQuery) == "undefined") {
+		window.setTimeout('window.fix_wmode2transparent_swf()', 100);
+		return;
+	}
+	if(window.noConflict){
+		jQuery.noConflict();
+	}
+	jQuery("embed:visible").not(".sIFR-flash").each(function(i) {
+		var elClone = this.cloneNode(true);
+		if (jQuery(elClone).attr("wmode") == null || jQuery(elClone).attr("wmode") == "window" || jQuery(elClone).attr("wmode") == "direct") {
+		  jQuery(elClone).attr("wmode", "transparent");  
+		}
+		jQuery(this).before(elClone);
+		jQuery(this).remove();
+	});	
+  jQuery("object:visible").not("#HeadlessAdManager").each(function() { 
+    var elClone = this.cloneNode(true);
+    if (jQuery(elClone).find("param[name=wmode]").attr("value") == null){
+      var param = jQuery("<param name='wmode' value='transparent'></param>");
+      param.appendTo(elClone);
+    } else if (jQuery(elClone).find("param[name=wmode]").attr("value") == "window" || jQuery(elClone).find("param[name=wmode]").attr("value") == "direct") {
+      jQuery(elClone).find("param[name=wmode]").attr("value", "transparent");
+    }
+  	jQuery(this).before(elClone);
+  	jQuery(this).remove();
+  });
+}
---
+++ fix_flash/fix_flash_ie8.js
@@ -0,0 +1,45 @@
+window.fix_wmode2transparent_swf = function () {
+	if(typeof (jQuery) == "undefined") {
+		window.setTimeout('window.fix_wmode2transparent_swf()', 200);
+		return;
+	}
+	if(window.noConflict)jQuery.noConflict();
+	// For embed
+	jQuery("embed:visible").not(".sIFR-flash").each(function(i) {
+		var elClone = this.cloneNode(true);
+		elClone.setAttribute("WMode", "Transparent");
+		jQuery(this).before(elClone);
+		jQuery(this).remove();
+	});	
+	// For object and/or embed into objects
+	jQuery("object:visible").not("#HeadlessAdManager").each(function (i, v) {
+	var elEmbed = jQuery(this).children("embed:visible");
+	if(typeof (elEmbed.get(0)) != "undefined") {
+		if(typeof (elEmbed.get(0).outerHTML) != "undefined") {
+			elEmbed.attr("wmode", "transparent");
+			jQuery(this.outerHTML).insertAfter(this);
+			jQuery(this).remove();
+		}
+		return true;
+	}
+	var algo = this.attributes;
+	var str_tag = '<OBJECT ';
+	for (var i=0; i < algo.length; i++) str_tag += algo[i].name + '="' + algo[i].value + '" ';	
+	str_tag += '>';
+	var flag = false;
+	jQuery(this).children().each(function (elem) {
+		if(this.nodeName == "PARAM") {
+			if (this.name == "wmode") {
+				flag=true;
+				str_tag += '<PARAM NAME="' + this.name + '" VALUE="transparent">';		
+			}
+			else  str_tag += '<PARAM NAME="' + this.name + '" VALUE="' + this.value + '">';
+		}
+	});
+	if(!flag)
+		str_tag += '<PARAM NAME="wmode" VALUE="transparent">';		
+	str_tag += '</OBJECT>';
+	jQuery(str_tag).insertAfter(this);
+	jQuery(this).remove();	
+	});
+}();
