535c51bfd363ec41f003b4e5b6d1fd547c048f73
Harris Novick <harris.novick@sonymusic.com>
2011-02-23 14:50:53 -0500

    Improved method of finding youtube iframe embeds.

--- fix_flash/fix_flash.js
+++ fix_flash/fix_flash.js
@@ -25,4 +25,13 @@ window.fix_wmode2transparent_swf = function () {
   	jQuery(this).before(elClone);
   	jQuery(this).remove();
   });
+  jQuery('iframe[src*="youtube"]').each(function(){
+      var url_fragment = jQuery(this).attr("src").split("?");
+      if (url_fragment[1] == undefined) {
+          var new_url = jQuery(this).attr("src") + "?wmode=Opaque"
+      } else {
+          var new_url = jQuery(this).attr("src") + "&wmode=Opaque"   
+      }
+      jQuery(this).attr("src", new_url);
+  });
 }
--- fix_flash/fix_flash_ie67.js
+++ fix_flash/fix_flash_ie67.js
@@ -51,4 +51,13 @@ window.fix_wmode2transparent_swf = function () {
           object.parentNode.removeChild(object);
       }
   }
+  jQuery('iframe[src*="youtube"]').each(function(){
+      var url_fragment = jQuery(this).attr("src").split("?");
+      if (url_fragment[1] == undefined) {
+          var new_url = jQuery(this).attr("src") + "?wmode=Opaque"
+      } else {
+          var new_url = jQuery(this).attr("src") + "&wmode=Opaque"   
+      }
+      jQuery(this).attr("src", new_url);
+  });
 }();
