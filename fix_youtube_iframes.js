ad7372e793f1aaea014d06a234d065c1ed6a07e9
Harris Novick <harris.novick@sonymusic.com>
2011-03-24 10:49:46 -0400

    fix_youtube_iframes.js

--- fix_youtube_iframes.js
+++ fix_youtube_iframes.js
@@ -1,9 +1,9 @@
-$("iframe.youtube-player").each(function(){
-    var url_fragment = $(this).attr("src").split("?");
-    if (url_fragment[1] == undefined) {
-        var new_url = $(this).attr("src") + "?wmode=Opaque"
-    } else {
-        var new_url = $(this).attr("src") + "&wmode=Opaque"   
-    }
-    $(this).attr("src", new_url);
+jQuery('iframe[src*="youtube"]').each(function(){
+  var url_fragment = jQuery(this).attr("src").split("?");
+  if (url_fragment[1] == undefined) {
+    var new_url = jQuery(this).attr("src") + "?wmode=Opaque"
+  } else {
+    var new_url = jQuery(this).attr("src") + "&wmode=Opaque"   
+  }
+  jQuery(this).attr("src", new_url);
 });
